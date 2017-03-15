fea8ff977b9d9d7a6bb71a2ddd02a8720fef365d
aotter115 <13594077248@163.com>
2017-03-15 12:58:40 +0800

    add

--- C++ 学习日志/externAndstatic.cpp
+++ C++ 学习日志/externAndstatic.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
 using namespace std;
 
-extern int i = 0;
-static int j = 0;
+extern int i = 0;//定义外部变量i
+static int j = 0;//同样可以定义j说明static有隐藏特性，防止在模块外被使用
--- C++ 学习日志/print.cpp
+++ C++ 学习日志/print.cpp
@@ -3,9 +3,21 @@ using namespace std;
 
 
 int fun();
-static int j;
+static int j;//静态全局变量
+
+static void fn();//声明静态函数
+void main()
+{
+	fn();
+}
+void fn()//定义静态函数
+{
+	int n = 10;
+	cout << n << endl;
+}
+
 void main(){
-	extern  int i;
+	extern  int i;//声明B.cpp中的i
 	cout << i << endl;
 	int j=fun();
 	cout << i << j << endl;
@@ -14,7 +26,19 @@ void main(){
 
 
 int fun() {
-	static int count = 10;
+	static int count = 10;//静态局部变量
 	return count--;
 	cout << count << endl;
 }
+
+
+class Myclass
+{
+public:
+	Myclass(int a, int b, int c);
+	void GetSum();
+private:
+	int a, b, c;
+	static int Sum;//声明静态数据成员
+	static void GetSum(); //声明静态成员函数
+};
